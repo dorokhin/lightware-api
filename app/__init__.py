dfe649a9cf38688700270bedd2c96fd36a5b2445
Andrew Dorokhin <andrew@dorokhin.moscow>
2019-01-13 20:26:04 +0700

    add authorization to all urls

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,20 @@ from .main.controller.auth_controller import api as auth_ns
 from .main.controller.channel_controller import api as channel_ns
 
 blueprint = Blueprint('api', __name__)
+authorizations = {
+    'Bearer Auth': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'Authorization'
+    },
+}
 
 api = Api(blueprint,
           title='LightWare RESTful API',
           version='1.0.0',
-          description='LightWare RESTful API server'
+          description='LightWare RESTful API server',
+          security='Bearer Auth',
+          authorizations=authorizations
           )
 
 api.add_namespace(user_ns, path='/user')
